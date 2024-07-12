e3ce53141791dc0864d5e40aa1bdf12d6902f8b1
Mel Pixel <melissapixell@gmail.com>
2024-07-12 17:50:59 +0300

    interface

---
+++ MelPixel/tasks/task-12.php
@@ -0,0 +1,32 @@
+<?php
+	interface iUser
+	{
+		public function setName($name); // установить имя 
+		public function getName();      // получить имя 
+		public function setAge($age);   // установить возраст 
+		public function getAge();       // получить возраст 
+	}
+
+    class User implements iUser{
+        private $name;
+        private $age;
+
+        public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function setAge($age){
+        $this->age = $age;
+    }
+    public function getAge()
+    {
+        return $this->age;
+    }
+    }
+?>
+
